dce4e8f14f152b2a444cdb4acc2063f0db0afa79
Prodgc <162835899+Prodgc@users.noreply.github.com>
2025-04-26 19:35:57 +0300

    Update testroblox.lua

--- testroblox.lua
+++ testroblox.lua
@@ -1,3 +1,4 @@
+-- NOT WORKS 1.
 local httpc = false -- DONT TOUCH
 pcall(function()
     if not script or not script.Source then
@@ -10,3 +11,18 @@ if httpsc then
 else
     print("source")
 end
+
+
+
+
+--second
+local function detectExecutionMethod()
+    local info = debug.getinfo(2)
+    if info and info.what == "Lua" then
+        print("source")
+    else
+        print("http")
+    end
+end
+
+detectExecutionMethod()
